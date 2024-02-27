a83efb54a6af3b613135b6dfd1d9cdd13a5152c0
Sandro Carneiro <81434865+Sandro176@users.noreply.github.com>
2024-02-27 08:34:15 -0300

    Add files via upload

---
+++ Escolaridade/src/Aluno.java
@@ -0,0 +1,10 @@
+public class Aluno extends Pessoa{
+
+    //CRIAR A CLASSE DO ALUNO
+
+    public Aluno(String cpf, String nome, int idade,String sexo){
+        super(cpf,nome,idade,sexo);
+    }
+
+
+}
